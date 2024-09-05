b3e8cb082da330d3669613734bf069f64d581d9b
Sandra Bastos <sandra@vermillion.net.br>
2024-09-05 14:04:18 -0300

    adds docs to clipboard.js

--- src/js/clipboard.js
+++ src/js/clipboard.js
@@ -1,5 +1,16 @@
 import { showToast } from "./toast.js";
 
+/**
+ * Copies a given text to the clipboard.
+ *
+ * Uses the Clipboard API if available. Returns a promise that resolves when the text
+ * is successfully copied or rejects with an error if the API is not available.
+ *
+ * @param {string} textToCopy - The text that will be copied to the clipboard.
+ * 
+ * @returns {Promise<void>} A promise that resolves if the text was copied 
+ * successfully, or rejects with an error if the Clipboard API is not available.
+ */
 export function copyTextToClipboard(textToCopy) {
     if (navigator?.clipboard?.writeText) {
         return navigator.clipboard.writeText(textToCopy);
@@ -7,6 +18,13 @@ export function copyTextToClipboard(textToCopy) {
     return Promise.reject(new Error('The Clipboard API is not available.'));
 }
 
+/**
+ * Sets up an event listener on a button to copy the password to 
+ * the clipboard when clicked.
+ *
+ * @param {string} buttonId - The ID of the button element that 
+ * triggers the copy action.
+ */
 export function passwordCopyEvent(buttonId) {
     const copyBtn = document.getElementById(buttonId);
     const password = document.getElementById("password").innerText;
