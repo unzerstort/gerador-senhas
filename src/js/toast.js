f75cdced8b6f98dbe8315129cf3d3222be6e47ec
Sandra Bastos <sandra@vermillion.net.br>
2024-09-05 13:51:27 -0300

    adds docs to toast.js

--- src/js/toast.js
+++ src/js/toast.js
@@ -1,9 +1,20 @@
+/**
+ * Displays a toast notification with a specified message and type.
+ *
+ * @param {string} message - The message to be displayed in the toast.
+ * @param {string} type - The type of the toast ('success' or 'error'). 
+ *                        This determines the toast's styling.
+ */
 export const showToast = (message, type) => {
     const seconds = 3000, 
     toast = document.getElementById("toast");
 
+    // Set the toast's specific class and message
     toast.className = `show font-medium toast-${type}`;
     toast.innerText = message;
 
-    setTimeout(() => { toast.className = toast.className.replace("show", " "); }, seconds);
+    // Hides the toast after the specified duration
+    setTimeout(() => { 
+        toast.className = toast.className.replace("show", " "); 
+    }, seconds);
 }
